72232fb522011ad4d93b64a94cb246f887c18c32
keepcalmandcodecodecode <igor.github@gmail.com>
2017-04-18 08:57:57 +0300

    SolveMeFirst

---
+++ Algorithms/Warmup/SolveMeFirst/SolveMeFirst/SolveMeFirst/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  SolveMeFirst
+//
+//  Created by developer on 4/18/17.
+//  Copyright © 2017 developer. All rights reserved.
+//
+
+import Foundation
+
+var a = Int(readLine()!)!
+var b = Int(readLine()!)!
+print (a+b)
