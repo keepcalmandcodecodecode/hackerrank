506b161fb6e4256319a8fc1403b59c328c92a16c
keepcalmandcodecodecode <igor.github@gmail.com>
2018-12-28 08:51:35 +0400

    complete DiagonalDifference

--- Algorithms/Warmup/DiagonalDifference/DiagonalDifference/main.swift
+++ Algorithms/Warmup/DiagonalDifference/DiagonalDifference/main.swift
@@ -2,38 +2,50 @@
 //  main.swift
 //  DiagonalDifference
 //
-//  Created by developer on 4/20/17.
-//  Copyright © 2017 developer. All rights reserved.
+//  Created by developer on 12/28/18.
+//  Copyright © 2018 developer. All rights reserved.
 //
 
 import Foundation
 
-import Foundation
-
-// read the integer n
-let n = Int(readLine()!)!
-
-// declare 2d array
-var arr: [[Int]] = []
-
-// read array row-by-row
-for _ in 0..<n {
-    arr.append(readLine()!.components(separatedBy: " ").map{ Int($0)! })
-}
-
-var primarySum = 0
-var secondarySum = 0
-
-for (rowIndex, rowArray) in arr.enumerated() {
-    for (columnIndex, columnElement) in rowArray.enumerated() {
-        if rowIndex == columnIndex {
-            primarySum = primarySum + columnElement
-        }
-        if (rowIndex + columnIndex)==(rowArray.count-1) {
-            secondarySum = secondarySum + columnElement
-        }
+// Complete the diagonalDifference function below.
+func diagonalDifference(arr: [[Int]]) -> Int {
+    let count = arr.count
+    var leftToRightSum = 0
+    var rightToLeftSum = 0
+    var rowIndex = 0
+    var columnOffset = 0
+    while rowIndex < count {
+        leftToRightSum = arr[rowIndex][columnOffset] + leftToRightSum
+        rightToLeftSum = arr[rowIndex][count-columnOffset-1]+rightToLeftSum
+        columnOffset = columnOffset + 1
+        rowIndex = rowIndex + 1
     }
+    return abs(leftToRightSum - rightToLeftSum)
 }
 
-let difference = abs(primarySum - secondarySum)
-print("\(difference)")
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+    else { fatalError("Bad input") }
+
+let arr: [[Int]] = AnyIterator{ readLine() }.prefix(n).map {
+    let arrRow: [Int] = $0.split(separator: " ").map {
+        if let arrItem = Int($0.trimmingCharacters(in: .whitespacesAndNewlines)) {
+            return arrItem
+        } else { fatalError("Bad input") }
+    }
+    
+    guard arrRow.count == n else { fatalError("Bad input") }
+    
+    return arrRow
+}
+
+guard arr.count == n else { fatalError("Bad input") }
+
+let result = diagonalDifference(arr: arr)
+
+fileHandle.write(String(result).data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
